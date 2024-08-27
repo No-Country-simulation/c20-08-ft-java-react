6ed6baf82770611e50c155ef3cf961b4575ae591
j0k3r <pdasriogallegos@gmail.com>
2024-08-27 10:49:25 -0300

    iniciando proyecto backend con java 17

---
+++ backend/src/test/java/com/c20_08_ft_java_react/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c20_08_ft_java_react.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
